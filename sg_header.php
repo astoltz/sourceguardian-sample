7819c676f9deb2a8f94783420f0bd09cf9e87899
Andrew M. Stoltz <1578013+astoltz@users.noreply.github.com>
2025-12-12 17:43:17 -0600

    Bump

--- sg_header.php
+++ sg_header.php
@@ -1,7 +1,7 @@
 <?php
 
 function sg_catchall($code, $message) {
-	require __DIR__ . DIRECTORY_SEPARATOR . 'sg_error_page.php';
+        require __DIR__ . DIRECTORY_SEPARATOR . 'sg_error_page.php';
         exit;
 }
 
